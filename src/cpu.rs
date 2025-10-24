e05b368e732c81f2805c55b54c758afff8ba4f26
Jordan Muir <jordanhmuir@gmail.com>
2025-10-24 12:15:36 -0700

    added new set target register fn, added 2 new instructions, INC and DEC

--- src/cpu.rs
+++ src/cpu.rs
@@ -13,7 +13,7 @@ impl CPU {
     fn execute(&mut self, instruction: Instructions) {
         match instruction {
             Instructions::ADD(target) => {
-                let register_value = self.get_target_register(target);
+                let register_value = self.get_target_register(&target);
                 let (result, did_overflow) = self.registers.a.overflowing_add(register_value);
                 self.set_flags_register(
                     result == 0, 
@@ -24,7 +24,7 @@ impl CPU {
                     // than the addition caused a carry from the lower nibble to the upper nibble
                     ((self.registers.a & 0xF) + (register_value & 0xF)) > 0xF
                 );
-                self.registers.a = result;
+                self.set_target_register(RegisterTarget::A, result);
             }
             Instructions::ADDHL(target) => {
                 let register_value = match target {
@@ -45,7 +45,7 @@ impl CPU {
             }
             Instructions::ADC(target) => {
                 let additional_carry = if self.registers.f.carry { 1 } else { 0 };
-                let register_value = self.get_target_register(target);
+                let register_value = self.get_target_register(&target);
                 let (result, did_overflow) = self.registers.a.overflowing_add(register_value);
                 let (new_result, new_did_overflow) = result.overflowing_add(additional_carry);
                 self.set_flags_register(
@@ -54,10 +54,11 @@ impl CPU {
                     new_did_overflow || did_overflow, 
                     ((self.registers.a & 0xF) + (register_value & 0xF) + additional_carry) > 0xF
                 );
-                self.registers.a = new_result;
+                self.set_target_register(RegisterTarget::A, new_result);
+                // self.registers.a = new_result;
             }
             Instructions::SUB(target) => {
-                let register_value = self.get_target_register(target);
+                let register_value = self.get_target_register(&target);
                 let (result, did_overflow) = self.registers.a.overflowing_sub(register_value);
                 self.set_flags_register(
                     result == 0, 
@@ -65,11 +66,12 @@ impl CPU {
                     did_overflow, 
                     (self.registers.a & 0xF) < (register_value & 0xF)
                 );
-                self.registers.a = result;
+                // self.registers.a = result;
+                self.set_target_register(RegisterTarget::A, result);
             }
             Instructions::SBC(target) => {
                 let additional_carry = if self.registers.f.carry { 1 } else { 0 };
-                let register_value = self.get_target_register(target);
+                let register_value = self.get_target_register(&target);
                 let (result, did_overflow) = self.registers.a.overflowing_sub(register_value);
                 let (new_result, new_did_overflow) = result.overflowing_sub(additional_carry);
                 self.set_flags_register(
@@ -78,10 +80,11 @@ impl CPU {
                     new_did_overflow || did_overflow, 
                     (self.registers.a & 0xF) < (register_value & 0xF) + additional_carry
                 );
-                self.registers.a = new_result;
+                self.set_target_register(RegisterTarget::A, new_result);
+                // self.registers.a = new_result;
             }
             Instructions::AND(target) => {
-                let register_value = self.get_target_register(target);
+                let register_value = self.get_target_register(&target);
                 let result = register_value & self.registers.a;
                 self.set_flags_register(
                     result == 0, 
@@ -89,10 +92,11 @@ impl CPU {
                     false, 
                     true
                 );
-                self.registers.a = result;
+                self.set_target_register(RegisterTarget::A, result)
+                // self.registers.a = result;
             }
             Instructions::OR(target) => {
-                let register_value = self.get_target_register(target);
+                let register_value = self.get_target_register(&target);
                 let result = register_value | self.registers.a;
                 self.set_flags_register(
                     result == 0, 
@@ -100,10 +104,11 @@ impl CPU {
                     false, 
                     false
                 );
-                self.registers.a = result;
+                self.set_target_register(RegisterTarget::A, result)
+                // self.registers.a = result;
             }
             Instructions::XOR(target) => {
-                let register_target = self.get_target_register(target);
+                let register_target = self.get_target_register(&target);
                 let result = register_target ^ self.registers.a;
                 self.set_flags_register(
                     result == 0, 
@@ -112,9 +117,10 @@ impl CPU {
                     false
                 );
                 self.registers.a = result;
+                self.set_target_register(RegisterTarget::A, result);
             }
             Instructions::CP(target) => {
-                let register_value = self.get_target_register(target);
+                let register_value = self.get_target_register(&target);
                 let (result, did_overflow) = self.registers.a.overflowing_sub(register_value);
                 self.set_flags_register(
                     result == 0,
@@ -122,6 +128,28 @@ impl CPU {
                     did_overflow,
                     (self.registers.a & 0xF) < (result & 0xF)
                 );
+            }
+            Instructions::INC(target) => {
+                let register_value = self.get_target_register(&target);
+                let (result, did_overflow) = register_value.overflowing_add(1);
+                self.set_flags_register(
+                    result == 0,
+                    false,
+                    did_overflow,
+                    ((result & 0xF) + (register_value & 0xF)) > 0xF
+                );
+                self.set_target_register(target, result);
+            }
+            Instructions::DEC(target) => {
+                let register_value = self.get_target_register(&target);
+                let (result, did_overflow) = register_value.overflowing_sub(1);
+                self.set_flags_register(
+                    result == 0,
+                    true,
+                    did_overflow,
+                    (register_value & 0xF) < (result & 0xF)
+                );
+                self.set_target_register(target, result);
             }
 
             _ => {
@@ -131,7 +159,7 @@ impl CPU {
 
     }
 
-    fn get_target_register(&self, target: RegisterTarget) -> u8 {
+    fn get_target_register(&self, target: &RegisterTarget) -> u8 {
         match target {
             RegisterTarget::A => self.registers.a,
             RegisterTarget::B => self.registers.b,
@@ -140,6 +168,20 @@ impl CPU {
             RegisterTarget::E => self.registers.e,
             RegisterTarget::H => self.registers.h,
             RegisterTarget::L => self.registers.l,
+        }
+    }
+
+    fn set_target_register(&mut self, target: RegisterTarget, value: u8) {
+        match target {
+            RegisterTarget::A => self.registers.a = value,
+            RegisterTarget::B => self.registers.b = value,
+            RegisterTarget::C => self.registers.c = value,
+            RegisterTarget::D => self.registers.d = value,
+            RegisterTarget::E => self.registers.e = value,
+            RegisterTarget::H => self.registers.h = value,
+            RegisterTarget::L => self.registers.l = value,
+
+
         }
     }
 
@@ -264,5 +306,19 @@ mod cpu_tests {
         assert_eq!(cpu.registers.a, 1);
         check_flags_register(cpu.registers.f, FlagsRegister{zero:false, subtract:true, carry: true, half_carry: true});
 
+    }
+    #[test]
+    fn test_inc() {
+        let mut cpu = use_test_cpu();
+        cpu.execute(Instructions::INC(RegisterTarget::H));
+        assert_eq!(cpu.registers.h, 7);
+        check_flags_register(cpu.registers.f, FlagsRegister{zero:false, subtract:false, carry:false, half_carry:false});
+    }
+    #[test]
+    fn test_dec() {
+        let mut cpu = use_test_cpu();
+        cpu.execute(Instructions::DEC(RegisterTarget::L));
+        assert_eq!(cpu.registers.l, 6);
+        check_flags_register(cpu.registers.f, FlagsRegister{zero:false, subtract:true, carry:false, half_carry:false});
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 mod cpu;
 mod macros;
 
-
 fn main() {
+    // let mut cpu = CPU::new();
     println!("Hello, world!");
 }
